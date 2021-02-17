d6e64bd17287c56487eaa17eb271e3eee3092ce9
muratbarlas <mlb771@nyu.edu>
2021-02-17 12:37:28 -0500

    part1 added

---
+++ hw3_part1/sketch.js
@@ -0,0 +1,83 @@
+var shapes = [];
+var numOfSquaresAcross = 20;
+var sizeOfSquare;
+var doingAnimation = true;
+
+
+
+function setup() {
+	createCanvas(1000, 500);
+	sizeOfSquare = width / numOfSquaresAcross;
+	background(100);
+	for (var x = 0; x < width+10; x += 70){
+    for (var y = height; y > -40; y -= 40){
+      shapes.push(new Shape(x,y))
+    }
+  }	
+  
+  
+}
+
+
+
+var rot_val  = 0;
+function draw() {
+ for(var i=0; i< shapes.length; i++){
+    
+    shapes[i].render();
+    shapes[i].changeColor();
+	}
+	
+	
+}
+
+class Shape{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+	
+    
+    this.tile_color_start = color(random(60,70),random(180, 240),random(200, 230))
+    this.tile_color_end = color(random(60,70),random(180, 240),random(200, 230))
+    this.val = 0;
+    
+    this.r=random(170, 255);
+		this.g = random(70, 80);
+    this.b = random(200, 210);
+  }
+
+  render() {
+		push()
+		var col_lerp = lerpColor(this.tile_color_start,this.tile_color_end,cos(this.val))
+    translate(this.x, this.y);
+		rotate(5.89);
+    ellipseMode(CENTER);
+    fill(col_lerp);
+		arc(0, 0, 80, 80, 0, PI + QUARTER_PI, CHORD);
+    
+    push();
+    rectMode(CENTER);
+		translate(-6, 14)
+    fill(192,192,192);
+    if (doingAnimation == true){
+      rotate(rot_val);
+      rot_val+=0.0001;
+      
+    }
+    else{rotate(rot_val)}
+		fill(this.r, this.g, this.b);
+		rect(0, 0, 20, 20);
+    
+   
+    
+    
+    pop();
+		pop()
+    
+  }
+
+  changeColor(){
+    this.val+=.08;
+  }
+	
+}
